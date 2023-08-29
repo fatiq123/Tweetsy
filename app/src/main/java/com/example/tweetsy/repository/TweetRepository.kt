0c785b0ba40458d233f6ce4aeb5fd87c9e5ff736
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-08-29 00:28:11 -0700

    repository and viewmodels code is implemenyted for project

---
+++ app/src/main/java/com/example/tweetsy/repository/TweetRepository.kt
@@ -0,0 +1,33 @@
+package com.example.tweetsy.repository
+
+import com.example.tweetsy.api.TweetsyApi
+import com.example.tweetsy.models.Tweet
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+class TweetRepository @Inject constructor(private val tweetsyApi: TweetsyApi) {
+
+    private val _categories = MutableStateFlow<List<String>>(emptyList())
+    val categories: StateFlow<List<String>>
+        get() = _categories
+
+    private val _tweets = MutableStateFlow<List<Tweet>>(emptyList())
+    val tweets: StateFlow<List<Tweet>>
+        get() = _tweets
+
+    suspend fun getCategories() {
+        val response = tweetsyApi.getCategories()
+        if (response.isSuccessful && response.body() != null) {
+            _categories.emit(response.body()!!)
+        }
+    }
+
+    suspend fun getTweets(category: String) {
+        val response = tweetsyApi.getTweets(category = category)
+        if (response.isSuccessful && response.body() != null) {
+            _tweets.emit(response.body()!!)
+        }
+    }
+
+}
